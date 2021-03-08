e55482b1e64db39587791197f2503b3daf17e6bf
nicopeis <nicopeis@gmail.com>
2021-03-08 10:12:30 +0100

    corrections

--- testShellExe.js
+++ testShellExe.js
@@ -3,9 +3,9 @@
 const shellExeIO = require('./shellExeIO.js');
 let testShellExeObj = new shellExeIO();
 
-testShellExeObj.commands = ['Load xfoilProfile', 'PLOP', 'G', '', 'PANE', 'OPER', 'CINC', 'pacc', 'out_Polar.txt', '', 'alfa 0', 'cpwr', 'out_Cp.txt', 'dump', 'out_Dump.txt', '', 'quit']
-testShellExeObj.exepath = __dirname + '/xfoil/xfoil.exe'
-testShellExeObj.cwdpath = __dirname + './xfoil'
+testShellExeObj.commands = ['Load xfoilProfile', 'PLOP', 'G', '', 'PANE', 'OPER', 'CINC', 'pacc', 'out_Polar.txt', '', 'alfa 0', 'cpwr', 'out_Cp.txt', 'dump', 'out_Dump.txt', '', 'quit'];
+testShellExeObj.exepath = __dirname + '/xfoil/xfoil.exe';
+testShellExeObj.cwdpath = __dirname + './xfoil';
 
 testShellExeObj.executeIO();
 
